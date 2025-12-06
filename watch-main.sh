4003e362b379184e7be66937a396ae8e136753e7
Martin <martinjonsson01@gmail.com>
2025-12-06 11:05:13 +0100

    Use separate stack dirs to prevent conflicts with HLS

--- watch-main.sh
+++ watch-main.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 ghcid \
-  --command "stack ghci hasskell-lib:lib hasskell-lib:test:hasskell-lib-test" \
+  --command "stack --work-dir .watch-stack-work ghci hasskell-lib:lib hasskell-lib:test:hasskell-lib-test" \
   --warnings \
   --test "Test.Syd.sydTest Hasskell.HomeAssistant.ClientSpec.spec" \
   --color=always
--- watch-test.sh
+++ watch-test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 ghcid \
-  --command "stack ghci hasskell-lib:lib hasskell-lib:test:hasskell-lib-test" \
+  --command "stack --work-dir .watch-stack-work ghci hasskell-lib:lib hasskell-lib:test:hasskell-lib-test" \
   --warnings \
   --test "Test.Syd.sydTest Hasskell.${1}.spec" \
   --color=always
