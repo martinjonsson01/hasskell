a5164883aefb34e5b2371c53fec4a8a233fa52b8
Martin <martinjonsson01@gmail.com>
2025-12-28 08:26:43 +0100

    Run tests with randomness by default

--- watch-test.sh
+++ watch-test.sh
@@ -1,12 +1,23 @@
 #!/usr/bin/env bash
-
 set -euo pipefail
 
-SPEC=${1}
+SPEC=$1
+SEED_ARG=${2:-}
+
+if [[ -n "${SEED_ARG}" ]]; then
+  # user passed --seed=n
+  # strip flag and read n
+  n="${SEED_ARG#--seed=}"
+  SEED_EXPR="(Test.Syd.Run.FixedSeed ${n})"
+else
+  # default
+  SEED_EXPR="Test.Syd.Run.RandomSeed"
+fi
 
 EXPR="Test.Syd.sydTestWith \
   (Test.Syd.OptParse.defaultSettings \
     { Test.Syd.OptParse.settingGoldenStart = Prelude.True \
+    , Test.Syd.OptParse.settingSeed = ${SEED_EXPR} \
     }) \
   ${SPEC}.spec"
 
